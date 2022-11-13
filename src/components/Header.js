1297071c48dc3398fc9625346ce3d84e10befd8d
ammar <ammarkhan575@gmail.com>
2022-11-14 03:03:02 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-left bold black mgLeft sz-5">🪙CoinBucket🪣</h1>
+    </div>
+  );
+};
+
+export default Header;
